9ced89bce9a063aefb78b3d4d8e29eae2b3c4820
Reza <rezapython@gmail.com>
2016-08-04 15:16:32 +0430

    base.html and list.html

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ def post_list(request):
                   'blog/post/list.html',
                   {'posts', posts})
 
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                              status='published',
