c5c33d6b58972368dccc033874f06b2e57aa6d9c
Reza <rezapython@gmail.com>
2016-08-04 14:42:29 +0430

    Creating model managers

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,13 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+            .filter(status='published')
+
+
 class Post(models.Model):
 
     STATUS_CHOICES = (
@@ -23,6 +30,8 @@ class Post(models.Model):
     status  = models.CharField(max_length=10,
                                choices=STATUS_CHOICES,
                                default='draft')
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
     class Meta:
         ordering = ('-publish',)
