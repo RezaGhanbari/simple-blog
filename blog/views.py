981802dc3bac8de9876240c09df3f991fb5c25d3
Reza <rezapython@gmail.com>
2016-08-05 18:51:38 +0430

    Comment system created.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,23 @@
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .forms import EmailPostForm
-from .models import Post
+from django.db.models import Count
+
+# from taggit.models import Tag
+
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
-def post_list(request, category=None):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    # if tag_slug:
+    #     tag = get_object_or_404(Tag, slug=tag_slug)
+    #     object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -35,7 +45,29 @@ def post_detail(request, year, month, day, post):
                                    publish__year=year,
                                    publish__month=month,
                                    publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    # List of similar posts
+
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'comment_form': comment_form,
+                                                     })
 
 
 def post_share(request, post_id):
@@ -52,7 +84,7 @@ def post_share(request, post_id):
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
             message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
-            send_mail(subject, message, 'rezapython@gmail.com', [cd['to']])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
             sent = True
     else:
         form = EmailPostForm()
