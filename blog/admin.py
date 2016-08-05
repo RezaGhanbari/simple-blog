a1f82c3f08714d4db1cc29419c0c4fc540c28650
Reza <rezapython@gmail.com>
2016-08-05 18:26:57 +0430

    Comment FormModel created.

--- blog/admin.py
+++ blog/admin.py
@@ -16,8 +16,8 @@ class PostAdmin(admin.ModelAdmin):
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'post', 'created', 'active')
-    list_filter = ('active', 'created', 'updated')
+    list_display  = ('name', 'email', 'post', 'created', 'active')
+    list_filter   = ('active', 'created', 'updated')
     search_fields = ('name', 'email', 'body')
 
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -51,13 +51,13 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments')
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
+    post    = models.ForeignKey(Post, related_name='comments')
+    name    = models.CharField(max_length=80)
+    email   = models.EmailField()
+    body    = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
+    active  = models.BooleanField(default=True)
 
     class Meta:
         ordering = ('created', )
